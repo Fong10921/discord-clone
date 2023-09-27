95a2fffe6cf1aecb6a4bc6e60613a0d724016449
Fong10921 <fongt277@gmail.com>
2023-09-27 16:54:40 +0800

    feat(EscButton): introduce new component and integrate with UserSetting
    - Create EscButton component for enhanced user interaction.
    - Adjust global.css to incorporate styling for the new component.
    - Implement the EscButton within the Layout for UserSetting.

---
+++ app/(userSetting)/components/EscButton.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import { XButtonSVG } from "@/components/SVG";
+import { useState } from "react";
+import Link from "next/dist/client/link";
+
+const ESCButton = () => {
+    const [isHovered, setHovered] = useState(false);
+
+    return (
+        <Link href="http://localhost:3000">
+            <div className="group mt-[4rem]"
+                onMouseEnter={() => setHovered(true)}
+                onMouseLeave={() => setHovered(false)}
+            >
+                <div className="closeButton-PCZcma border-zinc-400 group-hover:border-zinc-300 transition">
+                    <XButtonSVG hovered={isHovered} />
+                </div>
+                <p className="text-center text-bold text-sm mt-2 text-zinc-400 group-hover:text-zinc-300 transition">ESC</p>
+            </div>
+        </Link>
+    )
+}
+
+export default ESCButton
