77b6b8fdc41a88a11b13017f7399dde9b7032127
Fong <fongt277@gmail.com>
2023-10-01 20:24:41 +0800

    Update next config for Experiment Turbo.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    turbo: {
+      rules: {
+        '*.md': [
+          {
+            loader: '@mdx-js/loader',
+            options: {
+              format: 'md',
+            },
+          },
+        ],
+        '*.mdx': ['./my-mdx-loader'], // If you also want to use the my-mdx-loader for .mdx files
+      },
+    },
+  },
   webpack: (config) => {
     config.externals.push({
       "utf-8-validate": "commonjs utf-8-validate",
@@ -17,4 +32,4 @@ const nextConfig = {
   }
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
